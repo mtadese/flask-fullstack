58e1e87751fe3fc3998cccd85341d7a8fe7fb301
Ubuntu <ubuntu@ip-172-31-42-185.ec2.internal>
2022-01-01 18:51:48 +0000

    adding football route

--- demo.py
+++ demo.py
@@ -10,6 +10,13 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+#create a route for the flask app (football page)
+@app.route('/football', methods = ['GET'])
+
+#create a football function
+def football():
+	return render_template('football.html')
+
 #run the app on a preferred port
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port = 7000, debug= True)
